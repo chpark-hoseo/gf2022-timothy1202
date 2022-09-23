f09f92902418936f788460bbb13acb02c4537f94
timothy1202 <timothy1202@naver.com>
2022-09-23 12:58:19 +0900

    Gaem클래스화
    실습

---
+++ src/Game.cpp
@@ -0,0 +1,68 @@
+#include "Game.h"
+
+bool Game::init(const char *title, int xpos, int ypos, int width, int height, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        m_pWindow = SDL_CreateWindow(
+            title, xpos, ypos, width, height, flags);
+        if (m_pWindow != 0) {
+            m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+
+            if (m_pRenderer != 0) {
+                SDL_SetRenderDrawColor(
+                    m_pRenderer, 255, 255, 255, 255);
+            }
+            else {
+                return false; // 랜더러 생성 실패
+            }
+        }
+        else {
+            return false; // 윈도우 생설 실패 l
+        }
+    }
+    else {
+        return false; // SDL 초기화 실패
+    }
+
+    m_bRunning = true;
+    return true;
+}
+
+void Game::update()
+{
+    // 게임 데이터 갱신 
+}
+
+void Game::render()
+{
+    SDL_RenderClear(m_pRenderer);
+    SDL_RenderPresent(m_pRenderer);
+}
+
+bool Game::running()
+{
+    return m_bRunning;
+}
+
+void Game::handleEvents()
+{
+    SDL_Event event;
+    if (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            m_bRunning = false;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void Game::clean()
+{
+    SDL_DestroyWindow(m_pWindow);
+    SDL_DestroyRenderer(m_pRenderer);
+    SDL_Quit();
+}
---
+++ src/Game.h
@@ -0,0 +1,20 @@
+#include <SDL2/SDL.h> 
+
+class Game
+{
+public:
+	Game() { }
+	~Game() { }
+
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+	void render();
+	void update();
+	bool running();
+	void handleEvents();
+	void clean();
+
+private:
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+};
