972444963a12e2d048a136d96cc1fb8164eaa6a6
timothy1202 <timothy1202@naver.com>
2022-09-23 14:23:54 +0900

    SDL기본그리기
    실습 완성전

---
+++ src/Game.cpp
@@ -0,0 +1,62 @@
+﻿#include "Game.h"
+
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        m_pWindow = SDL_CreateWindow(
+            title, xpos, ypos, width, height, flags);
+        if (m_pWindow != 0) {
+            m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+
+            if (m_pRenderer != 0) {
+                SDL_SetRenderDrawColor(
+                    m_pRenderer, 255, 255, 255, 255);
+            }
+            else {
+                return false; // 랜더러 생성 실패
+            }
+        }
+        else {
+            return false; // 윈도우 생설 실패 l
+        }
+    }
+    else {
+        return false; // SDL 초기화 실패
+    }
+
+    m_bRunning = true;
+    return true;
+}
+
+typedef struct SDL_Texture
+{
+    SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+
+    m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+    SDL_FreeSurface(pTempSurface);
+}
+
+SDL_Rect m_sourceRectangle(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h)
+{
+    SDL_QueryTexture(m_pTexture, NULL, NULL,
+        &m_sourceRectangle.w, &m_sourceRectangle.h);
+}
+
+SDL_Rect m_destinationRectangle()
+{
+    m_destinationRectangle.w = m_sourceRectangle.w;
+    m_destinationRectangle.h = m_sourceRectangle.h;
+}
+
+
+	m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+}
+
+void Game::render()
+{
+    SDL_RenderClear(m_pRenderer);
+    SDL_RenderPresent(m_pRenderer);
+}
+
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
---
+++ src/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SDL2/SDL.h> 
+
+class Game
+{
+public:
+	Game() { }
+	~Game() { }
+
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+	void render();
+	void update();
+	bool running();
+	void handleEvents();
+	void clean();
+
+private:
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+	SDL_Texture* m_pTexture;
+
+	// 원본 사각형
+	SDL_Rect m_sourceRectangle;
+
+	// 대상 사각형
+	SDL_Rect m_destinationRectangle;
+};
