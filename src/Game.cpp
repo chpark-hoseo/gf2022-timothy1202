c81c45c82032c51c40905313629ccb512f103536
Park Kwang Hun <timothy1202@naver.com>
2022-09-25 21:38:35 +0900

    SDL기본그리기
    실습 최종

--- src/Game.cpp
+++ src/Game.cpp
@@ -24,39 +24,61 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
         return false; // SDL 초기화 실패
     }
 
+        SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+
+        m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+        SDL_FreeSurface(pTempSurface);
+
+        SDL_QueryTexture(m_pTexture, NULL, NULL,
+        &m_sourceRectangle.w, &m_sourceRectangle.h);
+
+        m_destinationRectangle.w = m_sourceRectangle.w;
+        m_destinationRectangle.h = m_sourceRectangle.h;
+
+        m_destinationRectangle.x = m_sourceRectangle.x = 0;
+        m_destinationRectangle.y = m_sourceRectangle.y = 0;
+
     m_bRunning = true;
     return true;
 }
 
-typedef struct SDL_Texture
+void Game::update()
 {
-    SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
-
-    m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-    SDL_FreeSurface(pTempSurface);
-}
-
-SDL_Rect m_sourceRectangle(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h)
-{
-    SDL_QueryTexture(m_pTexture, NULL, NULL,
-        &m_sourceRectangle.w, &m_sourceRectangle.h);
-}
-
-SDL_Rect m_destinationRectangle()
-{
-    m_destinationRectangle.w = m_sourceRectangle.w;
-    m_destinationRectangle.h = m_sourceRectangle.h;
-}
-
-
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+    // 게임 데이터 갱신 
 }
 
 void Game::render()
 {
     SDL_RenderClear(m_pRenderer);
+    SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
     SDL_RenderPresent(m_pRenderer);
 }
 
-	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+bool Game::running()
+{
+    return m_bRunning;
+}
+
+void Game::handleEvents()
+{
+    SDL_Event event;
+    if (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            m_bRunning = false;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void Game::clean()
+{
+    SDL_DestroyWindow(m_pWindow);
+    SDL_DestroyRenderer(m_pRenderer);
+    SDL_Quit();
+}
+
--- src/Game.h
+++ src/Game.h
@@ -25,4 +25,10 @@ private:
 
 	// 대상 사각형
 	SDL_Rect m_destinationRectangle;
+
+	typedef struct SDL_Rect
+	{
+		int x, y;
+		int w, h;
+	}SDL_Rect;
 };
