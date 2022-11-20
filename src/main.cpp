172886acc3c45003f4ca23ed8ecfffdbe5e42b81
timothy1202 <timothy1202@naver.com>
2022-11-20 14:00:36 +0900

    1
    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include "Game.h"
 #include<iostream>
 
+//Game* g_game = 0;
 const int FPS = 60;
 const int DELAY_TIME = 1000.0f / FPS;
 
 int main(int argc, char* argv[])
 {
-    Game* g_game = 0;
     Uint32 frameStart, frameTime;
     while (TheGame::Instance()->running()) {
         frameStart = SDL_GetTicks();     // step 1
@@ -19,7 +19,20 @@ int main(int argc, char* argv[])
         }
     }
 
+    if (TheGame::Instance()->init("Chapter 1",
+        100, 100, 640, 480, false))
+    {
+        while (TheGame::Instance()->running()) {
+            TheGame::Instance()->handleEvents();
+            TheGame::Instance()->update();
+            TheGame::Instance()->render();
+            SDL_Delay(10);
+        }
+    }
+    else {
+        std::cout << "game init failure " << SDL_GetError() << "\n";
+        return -1;
+    }
     TheGame::Instance()->clean();
     return 0;
 }
-
