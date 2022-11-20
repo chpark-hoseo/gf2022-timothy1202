144de72329f033a3ec0c53aa8f3c3c7a3189ca02
timothy1202 <timothy1202@naver.com>
2022-11-20 14:45:58 +0900

    물리 실습 완료
    최종

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,20 @@ const int DELAY_TIME = 1000.0f / FPS;
 
 int main(int argc, char* argv[])
 {
-    Uint32 frameStart, frameTime;
-    while (TheGame::Instance()->running()) {
-        frameStart = SDL_GetTicks();     // step 1
-        TheGame::Instance()->handleEvents();
-        TheGame::Instance()->update();
-        TheGame::Instance()->render();
-        frameTime = SDL_GetTicks() - frameStart;  // step 2
-        if (frameTime < DELAY_TIME) {
-            SDL_Delay((int)(DELAY_TIME - frameTime)); // step 3
-        }
-    }
 
     if (TheGame::Instance()->init("Chapter 1",
         100, 100, 640, 480, false))
     {
+        Uint32 frameStart, frameTime;
         while (TheGame::Instance()->running()) {
+            frameStart = SDL_GetTicks();     // step 1
             TheGame::Instance()->handleEvents();
             TheGame::Instance()->update();
             TheGame::Instance()->render();
-            SDL_Delay(10);
+            frameTime = SDL_GetTicks() - frameStart;  // step 2
+            if (frameTime < DELAY_TIME) {
+                SDL_Delay((int)(DELAY_TIME - frameTime)); // step 3
+            }
         }
     }
     else {
