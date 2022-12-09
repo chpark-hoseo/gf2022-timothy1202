adbfe75ec9e5686bf72f2d3abb9a7e0b1794a3be
timothy1202 <timothy1202@naver.com>
2022-12-09 21:12:07 +0900

    멈춤 구현완료

--- include/Player.h
+++ include/Player.h
@@ -5,22 +5,15 @@ class Player : public SDLGameObject
 {
 public:
     void handleInput();
-    void CrashUp();
-    void CrashDown();
-    void CrashRight();
-    void CrashLeft();
     Player(const LoaderParams* pParams);
     virtual void draw();
     virtual void update();
     virtual void clean();
     void move();
-    void start();
     static float Player_x;
     static float Player_y;
-    float Before_Player_x;
-    float Before_Player_y;
-    //∞‘¿” ∏ 
-    bool map[12][10] = { 1,1,1,1,1,1,1,1,1,1,1,1,
+    //∞‘¿” ∏ [y√‡][x√‡]
+    bool map[10][12] = { 1,1,1,1,1,1,1,1,1,1,1,1,
                          1,0,0,0,0,0,0,0,0,0,0,1,
                          1,0,1,1,0,0,0,0,0,0,0,1,
                          1,0,1,1,0,0,0,0,0,0,0,1,
@@ -32,5 +25,9 @@ public:
                          1,1,1,1,1,1,1,1,1,1,1,1 };
 private:
     int Player_Position_x=4;
-    int Player_Position_y=3;
+    int Player_Position_y=2;
+    float Before_Player_x = 400;
+    float Before_Player_y = 200;
+    void CheckPosition();
+    bool MoveNow=1;
 };
--- src/Player.cpp
+++ src/Player.cpp
@@ -8,12 +8,6 @@ float Player::Player_x;
 float Player::Player_y;
 Player::Player(const LoaderParams* pParams) : SDLGameObject(pParams) {}
 
-void Player::start()
-{
-    Before_Player_x= m_position.getX();
-    Before_Player_y = m_position.getY();
-}
-
 void Player::draw()
 {
     SDLGameObject::draw();
@@ -25,111 +19,68 @@ void Player::update()
 {
     Player_x = m_position.getX();
     Player_y = m_position.getY();
-    handleInput();
+    CheckPosition();
     move();
+    handleInput();
     m_currentFrame = ((SDL_GetTicks() / 100) % 4);
     SDLGameObject::update();
 }
 
 void Player::move()
 {
-    if (Before_Player_x == m_position.getX()+100)
+    if (Before_Player_x +100 == m_position.getX())
     {
         Player_Position_x++;
         Before_Player_x = m_position.getX();
-        std::cout << Player_Position_x;
+        std::cout << Player_Position_x << std::endl;
+        std::cout << Before_Player_x << std::endl;
     }
-    else if (Before_Player_y == m_position.getY() + 100) {
+    else if (Before_Player_y +100 == m_position.getY()) {
         Player_Position_y++;
         Before_Player_y= m_position.getY();
     }
 }
 
+void Player::CheckPosition()
+{
+    //오른쪽
+          if (map[Player_Position_y][Player_Position_x + 1] == 1)
+          {
+              MoveNow = 0;
+              if (MoveNow == 1)
+                  return;
+              else
+              m_velocity.setX(0);
+          }
+}
+
 void Player::handleInput()
 {
     if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT)) {
+        MoveNow = 1;
         m_velocity.setY(0);
         m_currentRow = 0;
-        if (map[Player_Position_x + 1][Player_Position_y] == 1)
-        {
-            m_velocity.setX(0);
-            std::cout << "hello";
-        }
-        else
-        {
-            m_velocity.setX(2);
-        }
-        //CrashRight();
+        m_velocity.setX(2);
     }
+
+
     else if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT)) {
+        MoveNow = 1;
         m_velocity.setY(0);
         m_velocity.setX(-2);
         m_currentRow = 1;
-        //CrashLeft();
+
     }
     else if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_UP)) {
         m_velocity.setX(0);
         m_velocity.setY(-2);
         m_currentRow = 2;
-        //CrashUp();
+
     }
     else if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_DOWN)) {
         m_velocity.setX(0);
         m_velocity.setY(2);
         m_currentRow = 3;
-        //CrashDown();
-    }
-}
-//고쳐야함 충돌 코드 이상함
-void Player::CrashUp() //위로 부딪힘
-{
-    if (m_position.getY() < 100)
-    {
-        m_position.setY(101);
-    }
 
-    else if (m_position.getY() < 400 && (m_position.getX() > 200 && m_position.getX() < 400))
-    {
-        m_velocity.setY(2);
-    }
-}
-
-void Player::CrashDown() //아래쪽으로 부딪힘
-{
-     if (m_position.getY() > 800)
-     {
-         m_position.setY(799);
-     }
-
-     else if (m_position.getY() > 100 && (m_position.getX() > 200 && m_position.getX() < 400))
-     {
-         m_velocity.setY(-2);
-     }
-}
-
-void Player::CrashRight() // 오른쪽으로 부딪힘 (좌표는 왼쪽위가 기준임을 알아야함)
-{
-    if (m_position.getX() > 1000)
-    {
-        m_position.setX(999);
-    }
-
-    else if ((m_position.getY() > 100 && m_position.getY() < 400) && m_position.getX() > 100)
-    {
-        m_position.setX(99);
-    }
-}
-
-void Player::CrashLeft() //왼쪽으로 부딪힘
-{
-
-    if (m_position.getX() < 100)
-    {
-        m_position.setX(101);
-    }
-
-    else if ((m_position.getY() > 100 && m_position.getY() < 400) && m_position.getX() < 400)
-    {
-        m_position.setX(401);
     }
 }
